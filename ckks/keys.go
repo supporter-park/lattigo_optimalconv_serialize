d7119f7d849c1aaa8848c46d6fb67dd7e7ceb494
supporter-park <5upporter.park@gmail.com>
2025-08-31 16:36:16 +0900

    fix: Add OnloadRtk to ckks.

--- ckks/keys.go
+++ ckks/keys.go
@@ -1,7 +1,14 @@
 package ckks
 
 // import "github.com/dwkim606/test_lattigo/rlwe"
-import "github.com/supporter-park/lattigo_optimalconv_serialize/rlwe"
+import (
+	"encoding/gob"
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/supporter-park/lattigo_optimalconv_serialize/rlwe"
+)
 
 // NewKeyGenerator creates a rlwe.KeyGenerator instance from the CKKS parameters.
 func NewKeyGenerator(params Parameters) rlwe.KeyGenerator {
@@ -36,4 +43,32 @@ func NewRelinearizationKey(params Parameters) *rlwe.RelinearizationKey {
 // (i.e., for each supported rotation).
 func NewRotationKeySet(params Parameters, galoisElements []uint64) *rlwe.RotationKeySet {
 	return rlwe.NewRotationKeySet(params.Parameters, galoisElements)
+}
+
+func OnloadRtk(params Parameters, rotations []int, tag string) (rtk *rlwe.RotationKeySet) {
+
+	rtk = new(rlwe.RotationKeySet)
+	// Match rotation to real index for each key
+	galEls := make([]uint64, len(rotations), len(rotations)+1)
+	for i, k := range rotations {
+		galEls[i] = params.GaloisElementForColumnRotationBy(k)
+	}
+	galEls = append(galEls, params.GaloisElementForRowRotation())
+
+	var swkl rlwe.SwitchingKeyLiteral
+	for _, g := range galEls {
+		rtk.Keys[g] = new(rlwe.SwitchingKey)
+		file, _ := os.Open("./rtk" + tag + "/" + strconv.FormatUint(g, 10) + ".gob")
+		defer file.Close()
+		decoder := gob.NewDecoder(file)
+		err := decoder.Decode(&swkl)
+		if err != nil {
+			fmt.Println("Rtk decoding error:", err)
+			return
+		}
+
+		rtk.Keys[g] = rlwe.NewSwitchingKeyFromLiteral(swkl)
+	}
+
+	return
 }
